4f6b1e551f4c25fa8d16d8aece58acf39a641e72
abhinav <itsabhinav5383@gmail.com>
2024-05-24 23:19:08 +0530

    Latest frontend build

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,53 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/bun";
 
+const frontendUrl = "http://localhost:3000";
+const backendUrl = "http://localhost:5500";
+
 const app = new Hono();
 
 app.use("/*", serveStatic({ root: "./dist" }));
+
+app.get("/api/*", async (c) => {
+	try {
+		const url = c.req.url;
+		const headers = c.req.raw.headers.toJSON();
+
+		return fetch(url.replace(frontendUrl, backendUrl), {
+			headers: headers,
+		});
+	} catch (e) {
+		return c.json({
+			success: false,
+			message: "Internal server error",
+		});
+	}
+});
+
+app.post("/api/*", async (c) => {
+	try {
+		const url = c.req.url;
+		const headers = c.req.raw.headers.toJSON();
+		const body = await c.req.json();
+
+		return fetch(url.replace(frontendUrl, backendUrl), {
+			method: "POST",
+			headers: headers,
+			body: JSON.stringify(body),
+		});
+	} catch (e) {
+		return c.json({
+			success: false,
+			message: "Internal server error",
+		});
+	}
+});
 
 app.get("*", (c) => {
 	return new Response(Bun.file("./dist/index.html"));
 });
 
 export default {
-	port: 5500,
+	port: 3000,
 	fetch: app.fetch,
 };
